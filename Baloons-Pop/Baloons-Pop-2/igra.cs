b4b8ac3bbf91edefa2cc777d3acf77e427a0e505
Lubo Gadjev <l.gadzhev@gmail.com>
2013-05-14 19:03:47 +0300

    Refacturing code
    rename variables, adding missing brackets, remove/add white spaces.

--- Baloons-Pop/Baloons-Pop-2/igra.cs
+++ Baloons-Pop/Baloons-Pop-2/igra.cs
@@ -4,31 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 
-//niamam obfuskator i zatova sym cepil na ryka, kojto mu se padne, da ne me tyrsi, zaminavam v cuzbina v kurmandirovka
 namespace BalloonsPops
 {
 	public class igra
 	{
-		const int shirina = 5;
-		const int length = 10;
+		const int width = 5;
+		const int height = 10;
 
-		private static int ost = shirina * length;
+		private static int ost = width * height;
 		private static int userMoves = 0;
 		private static int clearedCells = 0;
 
-
-
-		public static string[,] _t = new string[shirina, length];
+		public static string[,] _t = new string[width, height];
 		public static StringBuilder userInput = new StringBuilder();
 		private static SortedDictionary<int, string> statistics = new SortedDictionary<int, string>();
 
-		public static void Start()
+        /// <summary>
+        /// 
+        /// </summary>
+		public static void GameStart()
 		{
 			Console.WriteLine("Welcome to “Balloons Pops” game. Please try to pop the balloons. Use 'top' to view the top scoreboard, 'restart' to start a new game and 'exit' to quit the game.");
-			ost = shirina * length;
+			ost = width * height;
 			userMoves = 0;
-
-
 			clearedCells = 0;
 			CreateTable();
 			PrintTable();
@@ -37,9 +35,9 @@ namespace BalloonsPops
 
 		public static void CreateTable()
 		{
-			for (int i = 0; i < shirina; i++)
+			for (int i = 0; i < width; i++)
 			{
-				for (int j = 0; j < length; j++)
+				for (int j = 0; j < height; j++)
 				{
 					_t[i, j] = RandomGenerator.GetRandomInt();
 				}
@@ -50,11 +48,11 @@ namespace BalloonsPops
 			Console.WriteLine("    0 1 2 3 4 5 6 7 8 9");
 			Console.WriteLine("   ---------------------");
 
-			for (int i = 0; i < shirina; i++)
+			for (int i = 0; i < width; i++)
 			{
 				Console.Write(i + " | ");
 
-				for (int j = 0; j < length; j++)
+				for (int j = 0; j < height; j++)
 				{
 					Console.Write(_t[i, j] + " ");
 				}
@@ -64,19 +62,25 @@ namespace BalloonsPops
 
 			Console.WriteLine("   ---------------------");
 		}
+
 		public static void GameLogic(StringBuilder userInput)
 		{
 				PlayGame();
 				userMoves++;
 				userInput.Clear();
 				GameLogic(userInput);
-			
-
 		}
+
 		private static bool IsLegalMove(int i, int j)
 		{
-			if ((i < 0) || (j < 0) || (j > length - 1) || (i > shirina - 1)) return false;
-			else return (_t[i, j] != ".");
+			if ((i < 0) || (j < 0) || (j > height - 1) || (i > width - 1))
+            {
+                return false;
+            }
+			else
+            {
+                return (_t[i, j] != ".");
+            }
 		}
 
 		private static void InvalidInput()
@@ -91,8 +95,6 @@ namespace BalloonsPops
 			Console.WriteLine("Illegal move: cannot pop missing ballon!");
 			userInput.Clear();
 			GameLogic(userInput);
-
-
 		}
 
 		private static void ShowStatistics()
@@ -111,9 +113,7 @@ namespace BalloonsPops
 
 		private static void Restart()
 		{
-
-
-			Start();
+			GameStart();
 		}
 
 		private static void ReadTheIput()
@@ -125,33 +125,35 @@ namespace BalloonsPops
 			}
 			else
 			{
-				Console.Write("opal;aaaaaaaa! You popped all baloons in "+ userMoves +" moves."
+				Console.Write("opalaaaaaaaa! You popped all baloons in "+ userMoves +" moves."
 								 +"Please enter your name for the top scoreboard:");
 				userInput.Append(Console.ReadLine());
 				statistics.Add(userMoves, userInput.ToString());
 				PrintTheScoreBoard();
 				userInput.Clear();
-				Start();
+				GameStart();
 			}
 		}
 
 		private static void PrintTheScoreBoard()
 		{
-			int p = 0;
-
-
+			int standingsCount = 0;
 
 			Console.WriteLine("Scoreboard:");
-			foreach(KeyValuePair<int, string> s in statistics)
+			foreach(KeyValuePair<int, string> score in statistics)
 			{
-				if (p == 4) break;
+				if (standingsCount == 4)
+                {
+                    break;
+                }
 				else
 				{
-					p++;
-					Console.WriteLine("{0}. {1} --> {2} moves",p , s.Value, s.Key);
+					standingsCount++;
+					Console.WriteLine("{0}. {1} --> {2} moves",standingsCount , score.Value, score.Key);
 				}
 			}
 		}
+
 		private static void PlayGame()
 		{
 			int i = -1;
@@ -212,9 +214,9 @@ namespace BalloonsPops
 			int i; 
 			int j;
 			Queue<string> temp = new Queue<string>();
-			for (j = length-1; j >= 0; j--)
+			for (j = height-1; j >= 0; j--)
 			{
-				for (i = shirina-1; i >= 0; i--)
+				for (i = width-1; i >= 0; i--)
 				{
 					if (_t[i, j] != ".")
 					{
